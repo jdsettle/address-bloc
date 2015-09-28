ace408f0f29fd6ef306ee91058dbf4d6e895e20a
Jonathan Settle <jdsettle@gmail.com>
2015-09-28 19:57:18 -0400

    Completed Assignment 27

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - Delete all entries"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -36,6 +37,10 @@ class MenuController
       read_csv
       main_menu
     when 5
+      system "clear"
+      eraser
+      main_menu
+    when 6
       puts "Good-bye!"
       exit(0)
     else
@@ -160,6 +165,18 @@ class MenuController
   def delete_entry(entry)
     @address_book.entries.delete(entry)
     puts "#{entry.name} has been deleted"
+  end
+
+  def eraser
+    print "Are you sure you'd like to delete all entries? (y/n): "
+    selection = gets.chomp
+
+    case selection
+    when "y"
+      @address_book.entries.clear
+    when "n"
+      system "clear"
+    end
   end
 
   def edit_entry(entry)
